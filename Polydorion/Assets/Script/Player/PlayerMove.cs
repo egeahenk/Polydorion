6358edaa48bd0a968204dc3bade08a64da5afaeb
ege <egemenwho@gmail.com>
2023-09-03 16:08:48 +0300

    first person camera and player movement

---
+++ Polydorion/Assets/Script/Player/PlayerMove.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Movemenent")]
+    public float movSped;
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    public void Update()
+    {
+        MyInput();
+    }
+
+        private void FixedUpdate()
+        {
+            MovePly();
+        }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+    }
+
+    public void MovePly()
+    {
+        moveDirection =  orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * movSped * 10f, ForceMode.Force);
+    }
+
+}
