dd6ffe6cd6b130bc52ff783bbfd95a309e1f7494
ege <egemenwho@gmail.com>
2023-09-06 14:31:49 +0300

    changed 3rd person cam system

--- Polydorion/Assets/Script/Player/PlayerMove.cs
+++ Polydorion/Assets/Script/Player/PlayerMove.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public CharacterController cch;
+
+    public Transform cam;
+
     [Header("Movemenent")]
     public float movSped;
     public Transform orientation;
     float horizontalInput;
     float verticalInput;
 
+    public float smoothin = 0.1f;
+    float smoothinVelo;
+
     Vector3 moveDirection;
-
-
-
     Rigidbody rb;
+    
+ //   public Transform cam;
 
     public void Start()
     {
@@ -26,24 +32,53 @@ public class PlayerMove : MonoBehaviour
     {
         MyInput();
     }
-
-        private void FixedUpdate()
-        {
-            MovePly();
-        }
-
+/*
+    private void FixedUpdate()
+    {
+        MovePly();
+    }
+*/
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothinVelo, smoothin); 
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            cch.Move(moveDir.normalized * movSped * Time.deltaTime);
+        }
 
     }
-
+/*
     public void MovePly()
     {
         moveDirection =  orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized * movSped * 10f, ForceMode.Force);
     }
+*/
+    /*
+    public void cthing()
+    {
+        moveDirection = Quaternion.AngleAxis(cam.rotation.eulerAngles.y, Vector3.up) * moveDirection;
+        moveDirection.Normalize();
+    }
 
+    private void AppFocus(bool focus)
+    {
+        if(focus)
+        {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        }
+        else{
+            Cursor.lockState =  CursorLockMode.None;
+        }
+    }*/
 }
--- Polydorion/Assets/Script/Player/ThirdPersonCam.cs
+++ Polydorion/Assets/Script/Player/ThirdPersonCam.cs
@@ -4,10 +4,59 @@ using UnityEngine;
 
 public class ThirdPersonCam : MonoBehaviour
 {
-    
 
 
-    public float sensX;
+    [Header("Referances")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+    public float rotationSpeed;
+/*
+    public Transform combatLookAt;
+    public CameraStyle currentstyle;
+
+    public enum CameraStyle
+    {
+        Basic,
+        Combat,
+        Topdown,
+    }*/
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        //rotate orientation
+        
+        Vector3 viewDir = player.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        /*rotate player Object
+        /if(currentstyle == CameraStyle.Basic)
+        {*/
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+            if(inputDir != Vector3.zero)
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        //}
+
+        /*else if(currentstyle == CameraStyle.Combat)
+        {
+            Vector3 dirToCombatLookAt = combatLookAt.position = new Vector3(transform.position.x, player.position.y, transform.position.x);
+                orientation.forward = dirToCombatLookAt.normalized;
+
+            playerObj.forward = dirToCombatLookAt.normalized;
+        }*/
+    }
+
+    #region thing
+/*    public float sensX;
     public float sensY;
 
     public Transform orientation;
@@ -31,55 +80,7 @@ public class ThirdPersonCam : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
-    }   
-    
-    #region thing
-    /* [Header("Referances")]
-    public Transform orientation;
-    public Transform player;
-    public Transform playerObj;
-    public Rigidbody rb;
-    public float rotationSpeed;
-
-    public Transform combatLookAt;
-    public CameraStyle currentstyle;
-
-    public enum CameraStyle
-    {
-        Basic,
-        Combat,
-        Topdown,
-    }
-    public void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    private void Update()
-    {
-        //rotate orientation
-        Vector3 viewDir = player.position = new Vector3(transform.position.x, transform.position.y, transform.position.x);
-        orientation.forward = viewDir.normalized;
-
-        //rotate player Object
-        if(currentstyle == CameraStyle.Basic)
-        {
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
-            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-
-            if(inputDir != Vector3.zero)
-                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
-        }
-
-        else if(currentstyle == CameraStyle.Combat)
-        {
-            Vector3 dirToCombatLookAt = combatLookAt.position = new Vector3(transform.position.x, player.position.y, transform.position.x);
-                orientation.forward = dirToCombatLookAt.normalized;
-
-            playerObj.forward = dirToCombatLookAt.normalized;
-        }
-    }*/
+    }   */
     #endregion
+
 }
